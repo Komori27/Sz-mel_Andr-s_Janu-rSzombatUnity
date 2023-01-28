fd958b7116a55fabcd368d6398973ae58968a458
szamelandras <notwolfie@gmail.com>
2023-01-28 16:57:27 +0100

    a

---
+++ Assets/MyFirstUnityScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class MyFirstUnityScript : MonoBehaviour
+{
+    void Start()
+    {
+
+
+        Debug.Log("Hello Unity!");
+    }
+}
